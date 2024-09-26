cb719c0ccd72ad81961184a4d6cde84ca7461812
Apostolos Draganis <draganisdev@gmail.com>
2024-09-26 20:33:09 +0200

    Update bookingaroom.java

--- bookingaroom.java
+++ bookingaroom.java
@@ -12,19 +12,39 @@ public class bookingaroom
             if (r - n == 0) System.out.println("too late"); // If there are no available rooms
             else // If there are available rooms
             {
-                // Add unavailable rooms to a HashSet
-                HashSet<Integer> unavailableRooms = new HashSet<>();
-                for (int i = 0; i < n; i++) unavailableRooms.add(sc.nextInt());
+                // Solution using boolean array
+                boolean[] booked = new boolean[r + 1];
 
-                // Print room of the hotel if it does not exist unavailableRooms
+                // Mark booked rooms
+                for (int i = 0; i < n; i++)
+                    booked[sc.nextInt()] = true;
+
+                // Find the first available room
                 for (int i = 1; i <= r; i++)
-                {
-                    if (!unavailableRooms.contains(i))
+                    if (!booked[i])
                     {
                         System.out.println(i);
                         break;
                     }
-                }
+
+
+                
+                
+                
+                // Solution using Set
+//                // Add unavailable rooms to a HashSet
+//                HashSet<Integer> unavailableRooms = new HashSet<>();
+//                for (int i = 0; i < n; i++) unavailableRooms.add(sc.nextInt());
+//
+//                // Print room of the hotel if it does not exist unavailableRooms
+//                for (int i = 1; i <= r; i++)
+//                {
+//                    if (!unavailableRooms.contains(i))
+//                    {
+//                        System.out.println(i);
+//                        break;
+//                    }
+//                }
             }
         }
     }
